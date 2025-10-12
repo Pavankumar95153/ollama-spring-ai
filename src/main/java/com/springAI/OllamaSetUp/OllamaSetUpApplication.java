b636425be3cefd820ed6abe05316c57835bc2a14
Pavan Kumar <pavankumartvs123@gmail.com>
2025-10-12 21:36:46 +0530

    ollama first sample ai project

---
+++ src/main/java/com/springAI/OllamaSetUp/OllamaSetUpApplication.java
@@ -0,0 +1,13 @@
+package com.springAI.OllamaSetUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OllamaSetUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OllamaSetUpApplication.class, args);
+	}
+
+}
