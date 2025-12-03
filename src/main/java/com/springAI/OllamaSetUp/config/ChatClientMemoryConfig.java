114b68b78ba84d01c725c3e342659f3e79ccbfc5
Pavan Kumar <pavankumartvs123@gmail.com>
2025-12-03 18:29:29 +0530

    new class on chatMemory

---
+++ src/main/java/com/springAI/OllamaSetUp/config/ChatClientMemoryConfig.java
@@ -0,0 +1,23 @@
+package com.springAI.OllamaSetUp.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class ChatClientMemoryConfig {
+    @Bean("chatMemoryChatClient")
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory){
+        Advisor loggerAdvisor = new SimpleLoggerAdvisor();
+        Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
+        return chatClientBuilder
+                .defaultAdvisors(List.of(loggerAdvisor,memoryAdvisor)).build();
+    }
+}
